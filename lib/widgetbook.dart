50139f19038f0716332742279611eb3187da5414
Emre Reyhanlioglu <emre.reyhanlioglu@commencis.com>
2023-08-14 10:35:08 +0300

    Widgetbook: Add BrgTextFormField into reusable widgets catalog

---
+++ lib/widgetbook.dart
@@ -0,0 +1,33 @@
+import 'package:burgankuwait/widgetbook/reusable_widgets/widgetbook_brg_text_form_field.dart';
+import 'package:flutter/material.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+void main() {
+  runApp(const WidgetbookApp());
+}
+
+@widgetbook.App()
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Widgetbook.material(
+      directories: [
+        WidgetbookFolder(
+          name: 'Widgets',
+          children: [
+            WidgetbookFolder(
+              name: 'Reusable Widgets',
+              children: [
+                widgetbookBrgTextFormField,
+              ],
+            ),
+          ],
+        ),
+      ],
+      addons: [],
+    );
+  }
+}
